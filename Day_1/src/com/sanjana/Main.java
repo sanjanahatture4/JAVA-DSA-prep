8bbe6dc4a70701ac6349f8878f73d5539247902c
Sanjana <sanjana.hatture7171@gmail.com>
2023-08-08 23:40:47 +0530

    first commit

---
+++ Day_1/src/com/sanjana/Main.java
@@ -0,0 +1,14 @@
+package com.sanjana;
+
+import java.util.Arrays;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("HELLO WORLD");
+
+        int [] [] finalMarks= {{90,91,98},{68,63,69}};
+          System.out.println(finalMarks[1][2]);
+
+    }
+}
---
+++ Day_1/src/com/sanjana/Sumof2num.java
@@ -0,0 +1,15 @@
+package com.sanjana;
+
+import java.util.Scanner;
+
+public class Sumof2num {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+                int num1 = input.nextInt();
+                int num2 = input.nextInt();
+
+                int Sum = num1 + num2;
+        System.out.println("Sum is= " +  Sum);
+
+    }
+}
